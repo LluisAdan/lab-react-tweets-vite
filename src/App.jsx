2be12ea8d57f19a2cf88f72a71e7812497fe33bb
Lluis Adan <lluis.adan@gmail.com>
2024-04-04 13:38:46 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import "./App.css";
-import Tweet from "./components/Tweet";
+import Tweet from "./components/ui/Tweet";
 
 const tweetsArray = [
   {
@@ -40,7 +39,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={ tweetsArray[0] }/>
+      <Tweet tweet={ tweetsArray[1] }/>
+      <Tweet tweet={ tweetsArray[2] }/>
     </div>
   );
 }
